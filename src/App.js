9dcecb2db8fdc233d1269b82271f16521fe71e28
Dandalf <danielemon987@gmail.com>
2021-05-11 10:59:52 +1200

    Default values can be customised

--- src/App.js
+++ src/App.js
@@ -3,19 +3,22 @@ import { useEffect , useState, useCallback } from "react"
 import Header from "./components/Header"
 import Content from "./components/Content"
 
+// Default settings
+const defaultValues = {
+  testTime: 10,
+  leftKey: 'z',
+  rightKey: 'x',
+  streamFreq: 4, // 1 beat for every 1 semi-quaver. If 1, then it's per quaver
+}
+
 const App = () => {
-  const [totalTime, setTotalTime] = useState(10)
+  const settingValues = Object.create(defaultValues)
+  settingValues.testTime = 20
+
+  const [totalTime, setTotalTime] = useState(settingValues.testTime)
   const [timer, setTimer] = useState(totalTime)
   const [start, setStart] = useState(false)
   const [taps, setTaps] = useState(0)
-
-  // Default settings
-  const defaultValues = {
-    testTime: 10,
-    leftKey: 'z',
-    rightKey: 'x',
-    streamFreq: 4, // 1 beat for every 1 semi-quaver. If 1, then it's per quaver
-  }
 
   // Timer countdown
   useEffect(() => {
