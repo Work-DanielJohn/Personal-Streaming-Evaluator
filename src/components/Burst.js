807ddb678d8acb301b39a5bfd0aa91dda9b15d24
Dandalf <danielemon987@gmail.com>
2021-05-13 23:36:16 +1200

    Help section in each feature

--- src/components/Burst.js
+++ src/components/Burst.js
@@ -4,6 +4,7 @@ const Content = ({ title, time, settings, totalTime, taps }) => {
     return (
         <div>
             <h2>{title}</h2>
+            <button> Help </button>
             <StatsPanel time={time} settings={settings} totalTime={totalTime} taps={taps} />
         </div>
     )
