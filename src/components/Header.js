216e95cc0da1dac0258ac6f1a8f4369f450ea840
Dandalf <danielemon987@gmail.com>
2021-05-13 18:28:17 +1200

    Add and style header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,15 @@
 const Header = ({ title }) => {
     return (
         <header>
-            <h1>{title}</h1>
+            <div class="innerHeader">
+                <div class="title">
+                    <h1>{title}</h1>
+                </div>
+                <ul class="menuOptions">
+                    <button><li>Modes <i class="fas fa-gamepad"></i> </li></button>
+                    <button><li>Settings <i class="fas fa-cog"></i> </li></button>
+                </ul>
+            </div>
         </header>
     )
 }
