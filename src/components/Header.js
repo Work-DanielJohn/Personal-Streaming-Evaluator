a7736e73d5f66e5b63f8b097a9ff10d2738d9263
Dandalf <danielemon987@gmail.com>
2021-05-14 15:30:43 +1200

    Revert to old header hover reference

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,7 @@ const Header = ({ title }) => {
     return (
         <header>
             <button title="Streaming Modes"><i class="fas fa-2x fa-gamepad"></i></button>
-            <h1 title="PSE Burst!">PERS<span>O</span>NAL <span>S</span>TREAMING EVAL<span>U</span>ATOR<span>!</span></h1>
+            <h1 title="Tap those Beats!">PERS<span>O</span>NAL <span>S</span>TREAMING EVAL<span>U</span>ATOR<span>!</span></h1>
             <button title="Settings"><i class="fas fa-2x fa-cog"></i></button>
         </header>
     )
