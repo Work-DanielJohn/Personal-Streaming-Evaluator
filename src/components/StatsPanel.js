31fb712e53abc00947d7aec6aa39f4a5394fe536
Dandalf <danielemon987@gmail.com>
2021-05-14 15:30:58 +1200

    Reorder stats panel

--- src/components/StatsPanel.js
+++ src/components/StatsPanel.js
@@ -7,11 +7,11 @@ const StatsPanel = ({ time, settings, totalTime, taps }) => {
         <div>
             Time remaining: {time} <br></br>
             Time elapsed: {timeElapsed} <br></br>
+            Taps: {taps < 0 ? "0" : taps} <br></br>
             Avg streaming speed (taps/sec): {isNaN(averageStream) || !isFinite(averageStream)
                 ? "0" : averageStream.toFixed(2)} <br></br>
             Song bpm: {isNaN(songBPM) || !isFinite(songBPM)
-                ? "0" : songBPM.toFixed(2)} <br></br>
-            Taps: {taps < 0 ? "0" : taps}
+                ? "0" : songBPM.toFixed(2)} 
         </div>
     )
 }
