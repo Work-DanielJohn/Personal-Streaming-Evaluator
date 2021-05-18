b75441ab962413851ee8d991e0ad802bc4d5a32e
Dandalf <danielemon987@gmail.com>
2021-05-18 16:15:49 +1200

    Add settings component

--- src/components/Settings.js
+++ src/components/Settings.js
@@ -1,7 +1,13 @@
 const Settings = () => {
     return (
         <div>
-            
+            <div>
+                <h2>Settings</h2>
+            </div>
+            <p>1</p>
+            <p>2</p>
+            <p>3</p>
+            <p>4</p>
         </div>
     )
 }
